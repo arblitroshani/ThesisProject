c7b26c113946ea1d7f09aab631ca486f06eb09c1
Arbli Troshani <arblitroshani@gmail.com>
2018-06-24 16:53:40 +0200

    added progress dialog

--- app/src/main/java/me/arblitroshani/thesisproject/fragment/UploadFragment.java
+++ app/src/main/java/me/arblitroshani/thesisproject/fragment/UploadFragment.java
@@ -1,10 +1,10 @@
 package me.arblitroshani.thesisproject.fragment;
 
+import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +22,8 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -32,12 +34,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import me.arblitroshani.thesisproject.MeasureActivity;
 import me.arblitroshani.thesisproject.R;
 import me.arblitroshani.thesisproject.model.NameStat;
 
 public class UploadFragment extends Fragment implements View.OnClickListener {
 
     private static final String REALTIME_PATH = "namesUploadTest";
+    private static final String FIRESTORE_PATH = "namesTest";
 
     private static final String[] LABELS = {"Realtime Database", "Cloud Firestore", "Realm Platform"};
     private static final int[] LINE_COLORS = {Color.BLUE, Color.GREEN, Color.GRAY};
@@ -56,7 +60,7 @@ public class UploadFragment extends Fragment implements View.OnClickListener {
     private LineChart chart;
     private Button bRealtime, bFirestore, bRealm;
     private EditText etDataSet, etNumTrials;
-    private TextView tvAverageRealtime, tvAverageFirestore, tvAverageRealm;
+    private TextView tvAverage[];
 
     private List<ILineDataSet> dataSets;
     private LineData lineData;
@@ -64,8 +68,16 @@ public class UploadFragment extends Fragment implements View.OnClickListener {
     private LineDataSet[] dataSetUpload;
 
     private DatabaseReference realtimeRef;
+    private CollectionReference firestoreRef;
+
+    private InputStream inputStream;
+    private InputStreamReader inputReader;
+    private BufferedReader bufferedReader;
+
     private int dataSetSize;
     private int numTrials;
+    private NameStat ns;
+    private String line;
 
 
     public UploadFragment() {}
@@ -86,15 +98,15 @@ public class UploadFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         chart = view.findViewById(R.id.chart);
-
         bRealtime = view.findViewById(R.id.bRealtime);
         bFirestore = view.findViewById(R.id.bFirestore);
         bRealm = view.findViewById(R.id.bRealm);
         etDataSet = view.findViewById(R.id.etDataSet);
         etNumTrials = view.findViewById(R.id.etNumTrials);
-        tvAverageRealtime = view.findViewById(R.id.tvAverageRealtime);
-        tvAverageFirestore = view.findViewById(R.id.tvAverageFirestore);
-        tvAverageRealm = view.findViewById(R.id.tvAverageRealm);
+        tvAverage = new TextView[3];
+        tvAverage[REALTIME_INDEX] = view.findViewById(R.id.tvAverageRealtime);
+        tvAverage[FIRESTORE_INDEX] = view.findViewById(R.id.tvAverageFirestore);
+        tvAverage[REALM_INDEX] = view.findViewById(R.id.tvAverageRealm);
 
         bRealtime.setOnClickListener(this);
         bFirestore.setOnClickListener(this);
@@ -131,37 +143,47 @@ public class UploadFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         int id = view.getId();
 
-        switch (view.getId()) {
-            case R.id.bRealtime:
-                if (!getInput()) return;
+        int touchIndex;
 
-                createLineDataSet(REALTIME_INDEX);
-                entriesUpload[REALTIME_INDEX].clear();
+        if (id == R.id.bRealtime) touchIndex = REALTIME_INDEX;
+        else if (id == R.id.bFirestore) touchIndex = FIRESTORE_INDEX;
+        else if (id == R.id.bRealm) touchIndex = REALM_INDEX;
+        else return;
 
-                InputStream inputStream;
-                InputStreamReader inputReader;
-                BufferedReader bufferedReader;
+        if (!getInput()) return;
 
+        ProgressDialog pd = new ProgressDialog(getActivity());
+        pd.setTitle("Calculating " + LABELS[touchIndex] + "!");
+        pd.setMessage("Loading...");
+        pd.setIndeterminate(false);
+        pd.show();
+
+        createLineDataSet(touchIndex);
+        entriesUpload[touchIndex].clear();
+
+        Thread t = new Thread() {
+            @Override
+            public void run() {
                 double weightedAverage = 0.0;
-
                 try {
-                    for (int j = 0; j < numTrials + 1; j++) {
-                        realtimeRef.removeValue();
+                    for (int j = 0; j <= numTrials; j++) {
+                        if (touchIndex == 0) realtimeRef.removeValue();
+                        if (touchIndex == 1) firestoreRef = FirebaseFirestore.getInstance().collection(FIRESTORE_PATH + System.currentTimeMillis());
+
                         inputStream = getResources().openRawResource(R.raw.yob2017);
                         inputReader = new InputStreamReader(inputStream);
                         bufferedReader = new BufferedReader(inputReader);
-                        // get start time
+
                         long startTime = System.nanoTime();
-                        NameStat ns;
-                        String line;
                         for (int i = 0; i < dataSetSize; i++) {
                             line = bufferedReader.readLine();
                             List<String> list = Arrays.asList(line.split(","));
                             ns = new NameStat(list.get(0), 2017, Integer.parseInt(list.get(2)), list.get(1));
-                            realtimeRef.push().setValue(ns);
+                            if (touchIndex == 0) realtimeRef.push().setValue(ns);
+                            if (touchIndex == 1) firestoreRef.document().set(ns);
                         }
                         if (j == 0) continue;
-                        // get end time
+
                         long endTime = System.nanoTime();
                         double diff = (endTime - startTime) / 1000000.0;
                         if (j == 1) {
@@ -169,39 +191,26 @@ public class UploadFragment extends Fragment implements View.OnClickListener {
                         } else {
                             weightedAverage = 0.8 * weightedAverage + 0.2 * diff;
                         }
-                        // add to array
-                        entriesUpload[REALTIME_INDEX].add(new Entry((float) j, (float) diff));
+                        entriesUpload[touchIndex].add(new Entry((float) j, (float) diff));
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
 
-                tvAverageRealtime.setText(new DecimalFormat("#0.000").format(weightedAverage) + " ms");
+                final String toPrintAve = new DecimalFormat("#0.000").format(weightedAverage) + " ms";
 
-                dataSetUpload[REALTIME_INDEX].setValues(entriesUpload[REALTIME_INDEX]);
-                dataSets.set(REALTIME_INDEX, dataSetUpload[REALTIME_INDEX]);
+                getActivity().runOnUiThread(() -> tvAverage[touchIndex].setText(toPrintAve));
+
+                dataSetUpload[touchIndex].setValues(entriesUpload[touchIndex]);
+                dataSets.set(touchIndex, dataSetUpload[touchIndex]);
                 updateChart();
-
-                break;
-            case R.id.bFirestore:
-                if (!getInput()) return;
-
-                createLineDataSet(FIRESTORE_INDEX);
-                entriesUpload[FIRESTORE_INDEX].clear();
-
-                entriesUpload[FIRESTORE_INDEX].add(new Entry((float) 1.0, (float) 8.0));
-                entriesUpload[FIRESTORE_INDEX].add(new Entry((float) 2.0, (float) 10.0));
-
-                dataSetUpload[FIRESTORE_INDEX].setValues(entriesUpload[FIRESTORE_INDEX]);
-                dataSets.set(FIRESTORE_INDEX, dataSetUpload[FIRESTORE_INDEX]);
-                updateChart();
-
-                break;
-        }
+                pd.dismiss();
+            }
+        };
+        t.start();
     }
 
     private boolean getInput() {
-        // get inputs, check if within bounds
         dataSetSize = Integer.parseInt(etDataSet.getText().toString());
         numTrials = Integer.parseInt(etNumTrials.getText().toString());
         if (dataSetSize < DATASET_MIN || dataSetSize > DATASET_MAX) {
